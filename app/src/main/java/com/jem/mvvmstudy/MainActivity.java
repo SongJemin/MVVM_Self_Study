b4d253bb93cf85e16d6f927067d349a45d9f8e20
SongJemin <jemin56@naver.com>
2019-08-24 15:47:39 +0900

    Add onClickListener in xml file

--- app/src/main/java/com/jem/mvvmstudy/MainActivity.java
+++ app/src/main/java/com/jem/mvvmstudy/MainActivity.java
@@ -1,9 +1,11 @@
 package com.jem.mvvmstudy;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import com.jem.mvvmstudy.databinding.ActivityMainBinding;
@@ -14,25 +16,11 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
 
         final CounterViewModel viewModel = ViewModelProviders.of(this).get(CounterViewModel.class);
 
-        binding.mainCountTv.setText(viewModel.counter.getValue() + "");
+        binding.setViewModel(viewModel);
 
-        binding.mainAddFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewModel.increase();
-                binding.mainCountTv.setText(viewModel.counter.getValue() + "");
-            }
-        });
-
-        binding.mainRemoveFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewModel.decrease();
-                binding.mainCountTv.setText(viewModel.counter.getValue() + "");
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/jem/mvvmstudy/VmShareActivity.java
@@ -0,0 +1,13 @@
+package com.jem.mvvmstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VmShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vm_share);
+    }
+}
